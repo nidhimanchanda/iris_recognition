8259e3fb593291c31a38aae39b69b8d475ff8341
Oves Khan <weboves@gmail.com>
2013-08-23 11:10:25 +0530

    iris boundary detection

---
+++ iris_recog.m
@@ -0,0 +1,23 @@
+a=imread('C:\Users\savvy\Desktop\eye.jpg');
+image(a);
+i=rgb2gray(a);
+
+d=double(i)/150;
+j=d(1:4:end,1:4:end);
+
+G = fspecial('gaussian',[10 10],30);
+
+Ig = imfilter(j,G,'same');
+
+
+BW1 = edge(Ig,'prewitt');
+BW2 = edge(Ig,'canny');
+BW3 = bwareaopen(bw,30);
+
+
+figure
+subplot(1,2,1), imshow(BW2)
+subplot(1,2,2), imshow(BW3)
+truesize
+%imshow(Ig)
+
